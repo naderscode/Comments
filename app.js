a06d353d1c1fb9adf056885c308b88f7b2622a5e
naderscode <naderscode@gmail.com>
2018-12-17 14:58:50 -0500

    enable comments using the enter key

--- app.js
+++ app.js
@@ -9,10 +9,33 @@ var main = function() {
 			 	$new_comment = $("<p>").text($(".input").val());
 
 				$(".comments").append($new_comment);
+				$(".input").val("");
+
+			}
+	});
+
+$(".input").on("keypress", function(event){
+			var $new_comment;
+
+
+			if(event.keyCode == 13){
+
+			 if($(".input").val() !== "") {
+
+			 	$new_comment = $("<p>").text($(".input").val());
+
+				$(".comments").append($new_comment);
+				$(".input").val("");
+
+			}
+
 		}
+		
 	});
 
 
 };
+
+
 
 $(document).ready(main);
