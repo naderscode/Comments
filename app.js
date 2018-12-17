0ef88be854362d4244060f6938320049568beb8e
naderscode <naderscode@gmail.com>
2018-12-17 14:36:16 -0500

    disable empty comments

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ var main = function() {
 	"use strict"
 
 	$(".input-button").on("click", function(event){
-			var $new_comment = $("<p>").text($(".input").val());
+			var $new_comment;
 
-			$(".comments").append($new_comment);
+			 if($(".input").val() !== "") {
+
+			 	$new_comment = $("<p>").text($(".input").val());
+
+				$(".comments").append($new_comment);
+		}
 	});
 
 
